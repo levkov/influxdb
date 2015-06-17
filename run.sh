37a7681c3c4250f015053901d099669cab27285b
Evgeny Levkov <levkov@levkov.tk>
2015-06-17 07:49:43 -0400

    minor

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #/bin/sh
 
+ssh-keygen -f "/root/.ssh/known_hosts" -R [localhost]:222
 docker run -p 222:22 -p 8083:8083 -p 8086:8086 -p 9001:9001 --restart=always -d --name influxdb levkov/influxdb
